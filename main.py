bb1e4f73419e6754a5d6aac908eddf166fd83d39
Daniel C <dacruva@gmail.com>
2024-07-03 09:51:32 -0400

    Refactor UI and refactor password_generator function, added save passwords to a file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
+from random import choice, randint, shuffle
 from tkinter import *
+from tkinter import messagebox
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
            'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
@@ -8,41 +9,80 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '-', '_', '~', '.']
 
 
-def passwordgen():
+def password_generator():
     password = ""
     password_list = []
-    nr_letters = random.randrange(4, 6, 1)
-    nr_symbols = random.randrange(4, 6, 1)
-    nr_numbers = random.randrange(4, 6, 1)
+    # for letter in range(1, nr_letters + 1):
+    #     password_list.append(random.choice(letters))
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    password_list = password_symbols + password_numbers + password_letters
 
-    password_result_label.config(text="0")
+    shuffle(password_list)
+    password = "".join(password_list)
 
-    for letter in range(1, nr_letters + 1):
-        password_list.append(random.choice(letters))
-
-    for symbol in range(1, nr_symbols + 1):
-        password_list.append(random.choice(symbols))
-
-    for number in range(1, nr_numbers + 1):
-        password_list.append(random.choice(numbers))
-
-    random.shuffle(password_list)
-
-    for char in password_list:
-        password += char
-    password_result_label.config(text=f"{password}")
+    password_result_entry.insert(0, f"{password}")
     window.clipboard_clear()
     window.clipboard_append(password)
 
 
+def save2file():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_result_entry.get()
+
+    if not website or not password:
+        messagebox.showwarning(0, "Please don't leave any fields empty")
+
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"these are the details entered: \n"
+                                                      f"Email: {email} \nPassword: {password}\n Is it ok to save?")
+
+        if is_ok:
+            with open("supersecrets.txt", "a") as file:
+                file.write(f"{website} | {email} | {password}\n")
+
+            # Clearing entries
+            website_entry.delete(0, "end")
+            password_result_entry.delete(0, "end")
+
+
 window = Tk()
-window.title("Welcome to the PyPassword Generator!")
-window.config(padx=60, pady=40)
+window.title("PyPassword Generator!")
+window.config(padx=50, pady=50)
 
-password_result_label = Label(text="0")
-password_result_label.grid(column=1, row=2)
+# Canvas
+canvas = Canvas(width=200, height=200)
+logo = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo)
+canvas.grid(column=1, row=0)
 
-generate_button = Button(text="Generate password and copy to clipboard", command=passwordgen)
-generate_button.grid(column=1, row=1)
+# Labels
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+
+password_label = Label(text="Password")
+password_label.grid(column=0, row=3)
+
+# Entries
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
+email_entry = Entry(width=35)
+email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "@mail.com")
+password_result_entry = Entry(width=21)
+password_result_entry.grid(column=1, row=3)
+
+# Buttons
+generate_button = Button(text="Generate", command=password_generator)
+generate_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=36, command=save2file)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
