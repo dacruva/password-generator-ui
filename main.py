602174407f4d4df0bc98a5177be64850d23e062a
Daniel C <dacruva@gmail.com>
2024-07-05 14:18:02 -0400

    Changed from txt file to json file and added search functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from random import choice, randint, shuffle
 from tkinter import *
 from tkinter import messagebox
+import json
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
            'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
@@ -31,6 +32,12 @@ def save2file():
     website = website_entry.get()
     email = email_entry.get()
     password = password_result_entry.get()
+    json_format = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if not website or not password:
         messagebox.showwarning("No empty", "Please don't leave any fields empty")
@@ -39,15 +46,40 @@ def save2file():
         is_ok = messagebox.askokcancel(title=website, message=f"these are the details entered: \n"
                                                               f"Email: {email} \nPassword: {password}\n"
                                                               f" Is it ok to save?")
-
         if is_ok:
-            with open("supersecrets.txt", "a") as file:
-                file.write(f"{website} | {email} | {password}\n")
+            try:
+                with open("supersecrets.json", "r") as file:
+                    data = json.load(file)
 
-            # Clearing entries
-            website_entry.delete(0, "end")
-            password_result_entry.delete(0, "end")
+            except FileNotFoundError:
+                with open("supersecrets.json", "w") as file:
+                    json.dump(json_format, file, indent=4)
+            else:
+                data.update(json_format)
+                with open("supersecrets.json", "w") as file:
+                    json.dump(data, file, indent=4)
+            finally:
+                # Clearing entries
+                website_entry.delete(0, "end")
+                password_result_entry.delete(0, "end")
 
+
+def search():
+    website = website_entry.get()
+    try:
+        with open("supersecrets.json", "r") as file:
+            data = json.load(file)
+
+    except FileNotFoundError:
+        messagebox.showinfo(title=website, message=f"File not Found.")
+    else:
+        if website in data:
+            email = data[website]['email']
+            password = data[website]['password']
+            messagebox.showinfo(title=website, message=f"Email: {email} \n"
+                                                       f"Password: {password}\n")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exist.")
 
 window = Tk()
 window.title("PyPassword Generator!")
@@ -70,8 +102,8 @@ password_label = Label(text="Password")
 password_label.grid(column=0, row=3)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(column=1, row=2, columnspan=2)
@@ -85,5 +117,8 @@ generate_button.grid(column=2, row=3)
 
 add_button = Button(text="Add", width=36, command=save2file)
 add_button.grid(column=1, row=4, columnspan=2)
+
+search_button = Button(text="Search", width=8, command=search)
+search_button.grid(column=2, row=1)
 
 window.mainloop()
